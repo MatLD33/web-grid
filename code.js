3bfb2117c6c4e4e49b481c4c8d0d22d18f41d5de
Matteo Leturcq--Daligaux <matteo.leturcq--daligaux@etu.minesparis.psl.eu>
2023-03-01 15:00:05 +0100

    Step 1

--- code.js
+++ code.js
@@ -2,7 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
   // Initial clean up. DO NOT REMOVE.
   initialCleanup();
 
-  // Hey! Pssst! In here ...
+  let button = document.getElementById("btn-add-line")
+  button.addEventListener("click", () => {
+    console.log("C'est clické !")
+    let gridcube = document.getElementById("grid")
+    for (let i = 0; i < 10; i++) {
+      gridcube.appendChild(document.createElement('div'))
+    }
+  })
+
+  let gridcube = document.getElementById("grid")
+  for (let cube of gridcube.children) {
+    cube.addEventListener("click", () => {
+      console.log("Grille clické")
+      const random255 = () => Math.floor(Math.random() * 255)
+      const randomColor = () =>
+        `rgba(${random255()},${random255()},${random255()},1`
+      cube.style.backgroundColor = randomColor()
+    })
+  }
+
+
+
 });
 
 /**
